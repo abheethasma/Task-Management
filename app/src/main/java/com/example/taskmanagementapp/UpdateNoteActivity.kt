d3c5ede12d2095ee2e6e3ff81027491a93b95c09
abheethasma <148703787+abheethasma@users.noreply.github.com>
2024-05-12 20:31:32 +0530

    app version 1

---
+++ app/src/main/java/com/example/taskmanagementapp/UpdateNoteActivity.kt
@@ -0,0 +1,41 @@
+package com.example.taskmanagementapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.taskmanagementapp.databinding.ActivityUpdateBinding
+
+class UpdateNoteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityUpdateBinding
+    private lateinit var db: NotesDatabaseHelper
+    private var noteId: Int = -1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityUpdateBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = NotesDatabaseHelper(this)
+
+        noteId = intent.getIntExtra("note_id", -1)
+        if(noteId == -1){
+            finish()
+            return
+        }
+
+        val note = db.getNoteByID(noteId)
+        binding.updatetitleEditText.setText(note.title)
+        binding.updatecontentEditText.setText(note.content)
+
+        binding.updatesaveButton.setOnClickListener {
+            val newTitle = binding.updatetitleEditText.text.toString()
+            val newContent = binding.updatecontentEditText.text.toString()
+            val updateNote = Note(noteId, newTitle, newContent)
+            db.updateNote(updateNote)
+            finish()
+            Toast.makeText(this, "Changes Saved", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
